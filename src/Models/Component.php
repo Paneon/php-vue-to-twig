1936f562f8b1ba3356964efb2f0ffa9500b16c9f
Alexander Pape <a.pape@paneon.de>
2019-02-15 17:10:34 +0100

    Move Component, Property and Slot to Models/

--- src/Models/Component.php
+++ src/Models/Component.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Paneon\VueToTwig;
+namespace Paneon\VueToTwig\Models;
 
 
 use DOMDocument;
--- src/Models/Slot.php
+++ src/Models/Slot.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Paneon\VueToTwig;
+namespace Paneon\VueToTwig\Models;
 
 use Ramsey\Uuid\Uuid;
 
