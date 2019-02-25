27d07f679a239cb6f8a918483d9ae844ae2cdba9
Alexander Pape <a.pape@paneon.de>
2019-02-25 15:50:14 +0100

    Add space after dynamic class binding

--- tests/fixtures/vue-bind/double-attribute-binding.vue
+++ tests/fixtures/vue-bind/double-attribute-binding.vue
@@ -3,6 +3,9 @@
     <div :class="`dynamic dynamic--${modifier}`" class="static1 static2">
       Hello World
     </div>
+    <div :class="{ 'my-class': true }" class="static1 static2">
+      With Object Binding
+    </div>
   </div>
 </template>
 
