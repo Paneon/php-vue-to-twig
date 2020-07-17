91cfd649f81c86735e4b604f1d137065e928fc14
Stefan Hüsges <tronsha@gmx.de>
2020-07-17 07:43:38 +0200

    Fix expression code output in template strings

---
+++ tests/fixtures/vue-bind/binding-with-template-string-3.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="block">
+    <div :style="`width: ${100 / items.length}%;`">
+      Hello World
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: {
+        items: {
+              type: Array,
+              required: true,
+          },
+      },
+  };
+</script>
