700e3e00587332a1e2ff2d3d8004098550c8ff21
Stefan Hüsges <tronsha@gmx.de>
2020-07-31 11:01:57 +0200

    Add v-pre

--- tests/VuePreTest.php
+++ tests/VuePreTest.php
@@ -7,18 +7,34 @@ use Exception;
 class VuePreTest extends AbstractTestCase
 {
     /**
+     * @dataProvider dataProvider
+
+     *
      * @throws Exception
      */
-    public function testVueCloak()
+    public function testVuePre($template, $expected)
     {
-        $vueTemplate = '<template><div v-pre><div v-if="true">{{ 42 }}</div></div></template>';
-
-        $expected = '{% verbatim %}<div><div v-if="true">{{ 42 }}</div></div>{% endverbatim %}';
-
-        $compiler = $this->createCompiler($vueTemplate);
+        $compiler = $this->createCompiler($template);
 
         $actual = $compiler->convert();
 
         $this->assertEqualHtml($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return [
+            [
+                '<template><div v-pre><div v-if="true">{{ 42 }}</div></div></template>',
+                '{% verbatim %}<div><div v-if="true">{{ 42 }}</div></div>{% endverbatim %}',
+            ],
+            [
+                '<template><div v-pre v-if="true" class="foo"><h2 v-if="headline">{{ headline }}</h2><div class="bar"><Spinner><span v-if="button">{{ button }}</span></Spinner></div></div></template>',
+                '{% verbatim %}<div v-if="true" class="foo"><h2 v-if="headline">{{ headline }}</h2><div class="bar"><spinner><span v-if="button">{{ button }}</span></spinner></div></div>{% endverbatim %}',
+            ],
+        ];
     }
 }
