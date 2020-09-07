a6b90c87a4873e61166131f3c70e6412d601e0c2
Stefan Hüsges <tronsha@gmx.de>
2020-09-07 07:04:35 +0200

    Add test for attribute if true

---
+++ tests/AttributeIfTrueTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Paneon\VueToTwig\Tests;
+
+use Exception;
+
+class IfAttributesTest extends AbstractTestCase
+{
+    /**
+     * @dataProvider dataProvider
+     *
+     * @throws Exception
+     */
+    public function testAttributeIfTrue($template, $expected)
+    {
+        $compiler = $this->createCompiler($template);
+
+        $actual = $compiler->convert();
+
+        $this->assertEqualHtml($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProvider()
+    {
+        return [
+            [
+                '<template><form><input type="checkbox" :checked="false"></form></template>',
+                '<form class="{{ class|default(\'\') }}" style="{{ style|default(\'\') }}"><input type="checkbox" {% if false %}checked{% endif %}></form>',
+            ],
+        ];
+    }
+}
