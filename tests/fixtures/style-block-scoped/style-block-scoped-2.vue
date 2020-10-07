c71225bf5b791afa067d34eb9c0e97e37df41118
Stefan Hüsges <tronsha@gmx.de>
2020-10-07 13:53:22 +0200

    Add test for scoped styles

---
+++ tests/fixtures/style-block-scoped/style-block-scoped-2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="star"></div>
+</template>
+
+<style scoped>
+  .star {
+    margin-right: 0.125rem;
+    width: 3.125rem;
+    height: 3.125rem;
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D%220%200%20100%20100%22%20id%3D%22");
+  }
+  @media (-ms-high-contrast: none), (-ms-high-contrast: active) {
+    .star {
+      margin-right: -0.3125rem !important; }
+  }
+</style>
+
+<script>
+    export default {
+    }
+</script>
