b2d34d23dc6b7d8c26cfa02875e32d4a36b8bacb
Stefan Hüsges <tronsha@gmx.de>
2020-10-07 16:59:24 +0200

    Add test for binding with templatestring

---
+++ tests/fixtures/vue-bind/binding-with-template-string-4.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="block">
+    <div :style="foo ? foo : `width: ${size}; height: ${size}`">
+      Foo
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props: {
+        foo: {
+          type: String,
+          default: '',
+        },
+        size: {
+              type: Number,
+              required: true,
+          },
+      },
+  };
+</script>
