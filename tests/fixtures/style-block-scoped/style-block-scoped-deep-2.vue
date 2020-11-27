4fdce4791fcdf43a3aa4c028099726fbd0ab0b22
Stefan Hüsges <tronsha@gmx.de>
2020-11-27 10:44:13 +0100

    Fix regex for scoped styles

---
+++ tests/fixtures/style-block-scoped/style-block-scoped-deep-2.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <ul class="foo">
+    <li>foo</li>
+    <li>bar</li>
+    <li>baz</li>
+  </ul>
+</div>
+</template>
+
+<style scoped>
+  .foo /deep/ li:nth-child(2) {
+   color: purple;
+  }
+</style>
+
+<script>
+    export default {
+    }
+</script>
