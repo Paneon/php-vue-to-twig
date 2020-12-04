12981ea5185138d42dd728e95198c8844109cc20
Stefan Hüsges <tronsha@gmx.de>
2020-12-04 14:45:36 +0100

    Fix deep in scss

---
+++ tests/fixtures/style-block-scoped/style-block-scoped-deep-scss.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <div class="foo">
+    foo
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+  .foo {
+    color: red;
+    & /deep/ .bar {
+      color: blue;
+    }
+  }
+</style>
+
+<script>
+    export default {
+    }
+</script>
