be49f21318c42c55a276863eea4e8d2387b2a468
Stefan Hüsges <tronsha@gmx.de>
2020-12-23 11:19:48 +0100

    Add test for include binding

---
+++ tests/fixtures/vue-component/component-with-child-binding-2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ChildComponent :class="['foo', {'bar': true}, {'baz': false}]" />
+  </div>
+</template>
+<script>
+    export default {
+        name: 'component-with-child-binding.vue',
+        component: {
+            ChildComponent,
+        }
+    };
+</script>
